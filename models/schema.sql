9eb94f8ed0a20d9569996579b40caf3e8bb46360
Ryan Isaacson <munchinator6354@gmail.com>
2019-12-17 18:21:42 -0800

    removed Project2 file, including it's package-lock.json file and transferred the schema and seeds sql files over to our models file

--- models/schema.sql
+++ models/schema.sql
@@ -1,6 +1,12 @@
-DROP DATABASE IF EXISTS ratingsdb;
-CREATE DATABASE ratingsdb;
+DROP DATABASE IF EXISTS leaderboarddb;
+CREATE DATABASE leaderboarddb;
 
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE leaderboarddb
+CREATE TABLE leaderboard
+(
+	id int NOT NULL AUTO_INCREMENT,
+	username varchar(255) NOT NULL,
+    shape varchar(255) NOT NULL,
+	duration int NOT NULL,
+	PRIMARY KEY (id)
+);
